373848a1ce61877fef01ed486c5da62db088a244
Uğur Dağaşan <u.dagasan@hotmail.com>
2019-11-06 09:17:13 +0300

    Added-FindingShortStirngCountInsideOfLongString

---
+++ Process.Test/FindSubString.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Process.Test
+{
+    [TestClass]
+    public class FindSubString
+    {
+        [TestMethod]
+        public void When_Sperated_Word_It_Can_Be_Found()
+        {
+            //Case
+            var s1 = "Uğur Dağaşan Dağaşan Uğur ";
+            var s2 = "Dağaşan";
+
+            //When
+            var result = Process.FindSubString.Find(s1, s2);
+
+            //Then
+            Assert.AreEqual(result, 2);
+        }
+
+        [TestMethod]
+        public void When_Combined_It_Can_Be_Found()
+        {
+            //Case
+            var s1 = "Uğur DağaşanDağaşanDağaşanDağaşan";
+            var s2 = "Dağaşan";
+
+            //When
+            var result = Process.FindSubString.Find(s1, s2);
+
+            //Then
+            Assert.AreEqual(result, 4);
+        }
+        [TestMethod]
+        public void When_It_Can_Be_None_Case_Sensitive()
+        {
+            //Case
+            var s1 = "Uğur DAĞAŞANdağaşan";
+            var s2 = "Dağaşan";
+
+            //When
+            var result = Process.FindSubString.Find(s1, s2);
+
+            //Then
+            Assert.AreEqual(result, 2);
+        }
+
+    }
+}
---
+++ Process/FindSubString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Process
+{
+    public class FindSubString
+    {
+        public static int Find(string s1, string s2)
+        {
+            string source = string.Empty;
+            string value = string.Empty;
+
+            if (String.IsNullOrWhiteSpace(s2))
+                throw new ArgumentNullException(nameof(s2));
+            if (String.IsNullOrWhiteSpace(s1))
+                throw new ArgumentNullException(nameof(s1));
+            s1 = s1.ToUpper();
+            s2 = s2.ToUpper();
+
+
+            source = s1.Length >= s2.Length ? s1 : s2;
+            value = s1.Equals(source) ? s2 : s1;
+
+            var indexes = new List<int>();
+            for (int index = 0; ; index += value.Length)
+            {
+                index = source.IndexOf(value, index);
+                if (index == -1)
+                {
+                    return indexes.Count;
+                }
+                indexes.Add(index);
+            }
+        }
+    }
+}
