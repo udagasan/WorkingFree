9a604330bc2b47803078eeecb282763961b58989
Uğur Dağaşan <u.dagasan@hotmail.com>
2019-11-06 22:44:56 +0300

    DatabaseFileAdded

---
+++ DataAccessLayer/Dao.cs
@@ -0,0 +1,7 @@
+﻿namespace DataAccessLayer
+{
+    public class Dao
+    {
+        //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Users\udagasan\Source\Repos\Assigment\DataAccessLayer\Database.mdf;Integrated Security=True
+    }
+}
